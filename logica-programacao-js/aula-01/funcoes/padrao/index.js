f6decb844691ae33f9e2cd7475747428cae4d1b2
Rebeca Baptista <re.bfviana@gmail.com>
2022-02-04 16:43:30 -0300

    Cria pastas

--- logica-programacao-js/aula-01/funcoes/padrao/index.js
+++ logica-programacao-js/aula-01/funcoes/padrao/index.js
@@ -7,11 +7,14 @@ console.log(result);
 
 //Return como finalizador da função
 // Sempre que sua função encontra um return ela termina imediatamente. Sendo assim, nada após o return irá executar. Por isso que sempre colocamo-os na última linha.
+
+//Somar com parâmetros e retorno se os parâmetros são números
 function somar(num1, num2) {
   if (isNaN(num1) || isNaN(num2)) return NaN;
   return num1 + num2;
 }
 
+// Somar com parâmetros e retorno se os parâmetros são números (ternário)
 function somar(num1, num2) {
   return isNaN(num1) || isNaN(num2) ? NaN : num1 + num2;
 }
