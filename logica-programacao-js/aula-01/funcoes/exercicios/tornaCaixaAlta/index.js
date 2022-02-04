a559955e60e0d02f30f8e7a15acfd384e6837a87
Rebeca Baptista <re.bfviana@gmail.com>
2022-02-04 17:21:22 -0300

    Resolve exercicios

---
+++ logica-programacao-js/aula-01/funcoes/exercicios/tornaCaixaAlta/index.js
@@ -0,0 +1,4 @@
+// Crie uma função tornaCaixaAlta para converter qualquer texto para todas letras em maiúsculo
+
+const tornaCaixaAlta = (text) => console.log(text.toUpperCase());
+tornaCaixaAlta(prompt('Digite um texto para converter em letras maiúsculas:'));
