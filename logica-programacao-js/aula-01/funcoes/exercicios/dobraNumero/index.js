581f4c05caa6bdc1c2f30a3372e65bb61eeb6124
Rebeca Baptista <re.bfviana@gmail.com>
2022-02-07 17:43:07 -0300

    Altera valor da multiplicacao das funcoes

--- logica-programacao-js/aula-01/funcoes/exercicios/dobraNumero/index.js
+++ logica-programacao-js/aula-01/funcoes/exercicios/dobraNumero/index.js
@@ -1,17 +1,17 @@
 //7. Resolva o exercício 3 criando uma função dobraNumero que recebe um número como parâmetro e devolve seu dobro
 //    1. Resolva utilizando a sintaxe de function
 function dobraNumero1(num) {
-  return num * 3;
+  return num * 2;
 }
 const resultado = dobraNumero1(5);
 console.log(resultado);
 
 //    2. Resolva utilizando a sintaxe de arrow function
-const dobraNumero2 = (num) => num * 3;
+const dobraNumero2 = (num) => num * 2;
 const resultado2 = dobraNumero2(5);
 console.log(resultado2);
 
 //    3. Resolva agora deixando a função sem parâmetros e lendo o input do número do usuário nas duas sintaxes
-const dobraNumero3 = (num) => num * 3;
+const dobraNumero3 = (num) => num * 2;
 const resultado3 = dobraNumero3(Number(prompt('Digite um número:')));
 console.log(resultado3);
