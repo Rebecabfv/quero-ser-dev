10fcccd24d05e00b24f6aaab80d1d8b1f763e8ea
Rebeca Baptista <re.bfviana@gmail.com>
2022-02-16 15:26:27 -0300

    Adiciona exercicio sobre numero primo

---
+++ logica-programacao-js/aula-01/06-lacos-repeticao/exercio/numero-primo/index.js
@@ -0,0 +1,25 @@
+//Faça uma função que receba um número e verifique se ele é primo
+// (um número primo é apenas divisível por 1 e por ele mesmo)
+
+function verificaNumeroPrimo(num) {
+  if (num < 1) {
+    return false;
+  }
+
+  if (num === 1 || num === 2) {
+    return true;
+  }
+
+  let divisor = 2;
+
+  while (divisor < num) {
+    if (num % divisor === 0) {
+      return false;
+    }
+    divisor += 1;
+  }
+
+  return true;
+}
+
+console.log(verificaNumeroPrimo(2));
