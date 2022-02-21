ea230f939995c1232dee6a6ce68305ac321ed8bd
Rebeca Baptista <re.bfviana@gmail.com>
2022-02-21 17:09:13 -0300

    Atualiza conteudo sobre desestruturacao

--- logica-programacao-js/aula-01/09-desestruturacao/01-objetos/index.js
+++ logica-programacao-js/aula-01/09-desestruturacao/01-objetos/index.js
@@ -1,18 +1,44 @@
-const equipamentos = {
-  nome: 'Oculos de protecao',
+const equipamentoPadrao = {
+  nome: 'genérico',
   fabricante: '3M',
   tipo: 'epi',
+  dataDeValidade: 2,
 };
 
-// Extrair informacao com desestruracao
-const { fabricante, tipo } = equipamentos;
+// const fabricante = equipamentoPadrao.fabricante
+// const tipo = equipamentoPadrao.tipo
+// const nome = equipamentoPadrao.nome
+// const dataDeValidade = equipamentoPadrao.dataDeValidade
+
+const { fabricante, tipo, ...demaisAtributos } = equipamentoPadrao;
+
+// console.log(fabricante)
+// console.log(tipo)
+// console.log(demaisAtributos)
 
 const equipamento1 = {
-  nome: 'Oculos de protecao',
+  fabricante,
+  tipo,
+  ...demaisAtributos,
+};
+
+const equipamento2 = {
+  nome: 'luvas',
   fabricante,
   tipo,
 };
 
-console.log(fabricante);
-console.log(tipo);
-console.log(equipamento1);
+const equipamento3 = {
+  ...equipamento2, // spread operator
+  tipo: 'vestimenta',
+};
+
+const equipamento4 = {
+  nome: 'pé de pato',
+  fabricante: 'Bayard',
+  tipo: 'lazer',
+};
+
+// console.log(equipamento1)
+// console.log(equipamento2)
+console.log(equipamento3);
--- logica-programacao-js/aula-01/09-desestruturacao/01-objetos/readme.md
+++ logica-programacao-js/aula-01/09-desestruturacao/01-objetos/readme.md
@@ -1,19 +1,31 @@
+# Facilidades trazidas pelo ES6 (ES2015)
+
 # Desestruturação
 
 ## O que é?
 
-É uma forma de pegarmos propriedades de objetos ou posições de arrays de uma forma mais simples
+É uma forma de pegarmos propriedades dos objetos ou posições de arrays de uma forma mais simples
 
 ```js
-const equipamentos = {
-  nome: 'Oculos de protecao',
+const equipamento = {
+  nome: 'Óculos de proteção',
   fabricante: '3M',
   tipo: 'epi',
 };
-
-// Extrair informacao com desestruracao
-const { fabricante, tipo } = equipamentos;
-
+// const fabricante = equipamento.fabricante
+const { fabricante, tipo } = equipamento;
 console.log(fabricante);
 console.log(tipo);
 ```
+
+```js
+const equipamento2 = {
+  nome: 'pé de pato',
+  fabricante: 'Bayard',
+  tipo: 'lazer',
+};
+```
+
+# Rest operator (agrupa tudo que sobrar em um atributo)
+
+# Spread operator ()
