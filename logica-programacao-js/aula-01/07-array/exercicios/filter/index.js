a851b76efaf7e026376f6564bfacfe38fba0d95a
Rebeca Baptista <re.bfviana@gmail.com>
2022-02-21 17:04:54 -0300

    Adiciona exercicio com metodo filter

---
+++ logica-programacao-js/aula-01/07-array/exercicios/filter/index.js
@@ -0,0 +1,6 @@
+const numeros = [12, 15, 8, 130, 44];
+
+// use o metodo filter para gerar um array com os numeros maiores ou igual a 10
+const numerosMaioresQueDez = numeros.filter((numero) => numero >= 10);
+
+console.log(numerosMaioresQueDez);
