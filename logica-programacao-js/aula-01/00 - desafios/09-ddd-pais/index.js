0f271efd016c1f7e80f733a929a81f84b8d9143d
Rebeca Baptista <re.bfviana@gmail.com>
2022-02-24 17:23:13 -0300

    Adiciona exercicio

---
+++ logica-programacao-js/aula-01/00 - desafios/09-ddd-pais/index.js
@@ -0,0 +1,57 @@
+const telefones = [
+  {
+    pais: 'BR',
+    numero: '99235790',
+  },
+  {
+    pais: 'BR',
+    numero: '94342678',
+  },
+  {
+    pais: 'FR',
+    numero: '90357790',
+  },
+  {
+    pais: 'FIN',
+    numero: '99342578',
+  },
+  {
+    pais: 'MX',
+    numero: '88215857',
+  },
+  {
+    pais: 'LUX',
+    numero: '37654864',
+  },
+  {
+    pais: 'CL',
+    numero: '43647344',
+  },
+  {
+    pais: 'FR',
+    numero: '34646346',
+  },
+  {
+    pais: 'EG',
+    numero: '32632864',
+  },
+];
+
+const ddi = {
+  BR: 55,
+  CL: 56,
+  EG: 20,
+  FIN: 358,
+  FR: 33,
+  LUX: 352,
+  MX: 52,
+};
+
+const telefoneComDdd = telefones.map((contato) => {
+  const numero = ddi[contato.pais] + contato.numero;
+  return {
+    ...contato,
+    numero,
+  };
+});
+console.log(telefoneComDdd);
