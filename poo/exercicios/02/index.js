7536bb7457e20146443e6406400f33440d301da1
Rebeca Baptista <re.bfviana@gmail.com>
2022-03-03 17:35:00 -0300

    Melhora resolucao

--- poo/exercicios/02/index.js
+++ poo/exercicios/02/index.js
@@ -1,27 +1,39 @@
-// 2. Faça um objeto TV com as propriedades canal, volume e ligada e métodos liga, 
+// 2. Faça um objeto TV com as propriedades canal, volume e ligada e métodos liga,
 // desliga, mudaDeCanal e aumentaVolume, diminuiVolume.
 
 const tv = {
-  canal = "Globo",
-  volume = 20,
-  ligada = true,
+  canal: {
+    1: "Globo",
+    2: "SBT",
+    3: "Band",
+  },
+  volume: 20,
+  ligada: true,
   liga() {
     this.ligada = true;
   },
   desliga() {
-    this.ligada = falso;
+    this.ligada = false;
   },
-  mudaDeCanal (){
-    this.canel = "SBT";
+  mudaDeCanal(canal) {
+    if (this.liga) {
+      console.log("TV está desligada, não sendo possível alterar o canal.");
+      return;
+    }
+    this.canal = this.canal[canal];
   },
-  aumentaVolume(){
+  aumentaVolume() {
     this.volume += 2;
   },
-  diminuiVolume(){
+  diminuiVolume() {
     this.volume -= 2;
   },
-}
+};
 
+tv.desliga();
 
+tv.mudaDeCanal(2);
 
-// ["Globo","SBT", "Band"],
+tv.diminuiVolume();
+tv.diminuiVolume();
+console.log(tv);
