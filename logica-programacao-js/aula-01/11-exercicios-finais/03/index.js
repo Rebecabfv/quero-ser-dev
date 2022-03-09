bbd1108037ccdab3dc367b0251236db5cd9e0af2
Rebeca Baptista <re.bfviana@gmail.com>
2022-03-09 07:38:32 -0300

    Resolve problema

--- logica-programacao-js/aula-01/11-exercicios-finais/03/index.js
+++ logica-programacao-js/aula-01/11-exercicios-finais/03/index.js
@@ -1,13 +1,32 @@
-// Permita a um usuário jogar até 4 dados simultâneos que tenham de 4, 6, 8, 10, 12 ou 20 e retorne o valor total do número obtido da seguinte forma:
+// Permita a um usuário jogar até 4 dados simultâneos que tenham de 4, 6, 8, 10, 12 ou 20 e
+// retorne o valor total do número obtido da seguinte forma:
 // Número de dados escolhidos: 3
 // Valor sorteado do primeiro dado de 4 lados: 1
 // Valor sorteado do segundo dado de 8 lados: 7
 // Valor sorteado do terceiro dado de 12 lados: 12
 // Valor total: 20
 
-const numeroDeDados = prompt('Digite o numero de dados que deseja: ');
+const numeroDeDados = prompt('Digite o numero de dados que deseja: (1 ate 4)');
+let dadosSorteados;
+let soma = 0;
+const numeros = [4, 6, 8, 10, 12, 20];
 
-for (let i = numeroDeDados; i > 0; i--) {
-  let ladoDados = Math.floor(4, 6, 8, 10, 12, 20);
-  console.log(ladoDados);
+for (let i = 1; i <= numeroDeDados; i++) {
+  const numero = Math.floor(Math.random() * numeros.length);
+  dadosSorteados = numeros[numero];
+  numeros.splice(numero, 1);
+
+  const primeiroDado = Math.floor(Math.random() * dadosSorteados + 1);
+  console.log(
+    'Valor sorteado do ' +
+      i +
+      'º dado de ' +
+      dadosSorteados +
+      ' lados: ' +
+      primeiroDado
+  );
+
+  soma = soma + primeiroDado;
 }
+
+console.log('Valor total: ' + soma);
