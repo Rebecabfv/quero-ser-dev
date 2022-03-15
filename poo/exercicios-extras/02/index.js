b1f2fd9bfae07df912e2d9ba27a2a0998e439824
Rebeca Baptista <re.bfviana@gmail.com>
2022-03-15 09:13:02 -0300

    Melhora resolucao

--- poo/exercicios-extras/02/index.js
+++ poo/exercicios-extras/02/index.js
@@ -5,12 +5,50 @@
 
 class SmartWatch {
   #passosDiarios;
+  #horarioAtual;
 
   constructor(passosDiarios) {
     this.passosDiarios = passosDiarios;
+    const data = new Date();
+    this.#horarioAtual = data.getHours();
   }
 
   get passosDiarios() {
     return this.#passosDiarios;
   }
+
+  set passosDiarios(passosDiarios) {
+    if (this.#passosDiarios === undefined) {
+      this.#passosDiarios = passosDiarios;
+      return;
+    }
+    if (this.#passosDiarios !== undefined) {
+      this.#passosDiarios =
+        passosDiarios + this.#passosDiarios > this.#passosDiarios
+          ? passosDiarios + this.#passosDiarios
+          : "passos nao pode ser decrementado";
+      return;
+    }
+  }
+
+  resetOnMidnight() {
+    this.#passosDiarios = 0;
+    // Quando for meia noite ele zera a contagem de passos
+    if (this.#horarioAtual === 24) {
+      this.#passosDiarios = 0;
+    }
+  }
 }
+
+const relogio = new SmartWatch(4);
+
+console.log(relogio.passosDiarios);
+console.log(relogio);
+relogio.resetOnMidnight();
+console.log(relogio);
+relogio.passosDiarios = 5;
+console.log(relogio);
+relogio.passosDiarios = 5;
+console.log(relogio);
+relogio.resetOnMidnight();
+console.log(relogio);
