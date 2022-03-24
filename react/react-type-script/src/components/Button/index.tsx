84705c31768311ed4f77144ff0fabf0598f685b7
Rebeca Baptista <re.bfviana@gmail.com>
2022-03-24 15:36:25 -0300

    Adiciona renderizacao condicional

--- react/react-type-script/src/components/Button/index.tsx
+++ react/react-type-script/src/components/Button/index.tsx
@@ -2,11 +2,12 @@ import { FaSpinner, FaThumbsUp } from "react-icons/fa";
 import "./styles.css";
 
 export function Button() {
+  const temTexto = false;
   return (
     <button className="default-btn">
       {/* <sbg></sbg> */}
       <FaThumbsUp />
-      <p>Text only</p>
+      {temTexto ? <p>Text only</p> : <></>}
     </button>
   );
 }
