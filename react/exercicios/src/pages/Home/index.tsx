30c113174eb8bd41e83e484270e057164ff4a0ad
Rebeca Baptista <re.bfviana@gmail.com>
2022-03-28 16:53:40 -0300

    Add styles.css in Home

--- react/exercicios/src/pages/Home/index.tsx
+++ react/exercicios/src/pages/Home/index.tsx
@@ -1,8 +1,9 @@
+import "./styles.css";
 import { Link } from "react-router-dom";
 
 export function Home() {
   return (
-    <div>
+    <div className="links-container">
       <Link to="/chat">Go to Chat</Link>
       <hr />
       <Link to="/gallery">Go to Image Gallery</Link>
