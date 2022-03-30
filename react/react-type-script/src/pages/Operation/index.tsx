7a62045316e162dd86d7150627fca728f4fd4fe3
Rebeca Baptista <re.bfviana@gmail.com>
2022-03-30 17:26:40 -0300

    fix input user

--- react/react-type-script/src/pages/Operation/index.tsx
+++ react/react-type-script/src/pages/Operation/index.tsx
@@ -4,16 +4,19 @@ import "./styles.css";
 export function OperationSum() {
   const [soma, setSoma] = useState(0);
   const [historico, setHistorico] = useState([] as number[]);
+  const [numero1, setNumero1] = useState(0);
+  const [numero2, setNumero2] = useState(0);
 
   function handleSomar(event: React.FormEvent<HTMLFormElement>) {
     // nao deixar a pag carregar novamente
     event.preventDefault();
 
-    const numero1Input = event.currentTarget.querySelector("#number-one");
-    const numero1 = Number((numero1Input as any).value);
+    // Resolucao usando APIs de JavaScript Vanilla (querySelector)
+    // const numero1Input = event.currentTarget.querySelector("#number-one");
+    // const numero1 = Number((numero1Input as any).value);
 
-    const numero2Input = event.currentTarget.querySelector("#number-two");
-    const numero2 = Number((numero2Input as any).value);
+    // const numero2Input = event.currentTarget.querySelector("#number-two");
+    // const numero2 = Number((numero2Input as any).value);
 
     setSoma(numero1 + numero2);
 
@@ -29,12 +32,22 @@ export function OperationSum() {
       <form action="" onSubmit={handleSomar} className="math-operation">
         <div>
           <label htmlFor="number-one">Primeiro número</label>
-          <input type="text" id="number-one" />
+          <input
+            type="text"
+            id="number-one"
+            value={numero1}
+            onChange={(event) => setNumero1(Number(event.target.value))}
+          />
         </div>
 
         <div>
           <label htmlFor="number-two">Segundo número</label>
-          <input type="text" id="number-two" />
+          <input
+            type="text"
+            id="number-two"
+            value={numero2}
+            onChange={(event) => setNumero2(Number(event.target.value))}
+          />
         </div>
 
         {/* <button type="button" onClick={handleSomar}>
