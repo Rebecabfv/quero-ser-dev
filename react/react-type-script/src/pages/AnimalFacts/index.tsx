f234c6dca4915280c6f67d559f5eed8f2fcdcd70
Rebeca Baptista <re.bfviana@gmail.com>
2022-04-04 15:41:19 -0300

    fix page AnimalFacts

--- react/react-type-script/src/pages/AnimalFacts/index.tsx
+++ react/react-type-script/src/pages/AnimalFacts/index.tsx
@@ -1,40 +1,97 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+
+type Fact = {
+  _id: string;
+  text: string;
+  updatedAt: Date;
+};
 
 export function AnimalFacts() {
-  const [dafaultFacts, setDefaultFacts] = useState([]);
-  async function getRandomFact() {
-    const animalType = "dog";
-    const amount = 2;
+  // const [defaultFacts, setDefaultFacts] = useState([] as Fact[])
+  const [defaultFacts, setDefaultFacts] = useState<Fact[]>([]);
+  const [facts, setFacts] = useState<Fact[]>([]);
+  // let defaultFacts = []
+  const [searchedAnimalType, setSearchedAnimalType] = useState("");
+  const [animalType, setAnimalType] = useState("");
 
+  async function getDefaultRandomFacts() {
     const response = await fetch(
-      `https://cat-fact.herokuapp.com/facts/random?${animalType}_type=cat&amount=${amount}`,
-      {
-        method: "GET",
-      }
+      `https://cat-fact.herokuapp.com/facts/random?animal_type=dog&amount=2`
     );
 
-    const facts = await response.json();
-    console.log(facts);
+    const retrievedFacts = await response.json();
 
-    const factsTexts = facts.map((fact: any) => fact.text);
-    console.log(factsTexts);
+    setDefaultFacts(retrievedFacts);
+    // defaultFacts = retrievedFacts
+  }
 
-    setDefaultFacts(factsTexts);
+  async function getRandomFacts(animal_type: string = "dog") {
+    const response = await fetch(
+      `https://cat-fact.herokuapp.com/facts/random?animal_type=${animal_type.toLowerCase()}&amount=5`
+    );
+
+    const retrievedFacts = await response.json();
+
+    setAnimalType(searchedAnimalType);
+    setFacts(retrievedFacts);
+    // defaultFacts = retrievedFacts
+  }
+
+  function handleSubmitAnimalFactSearch(
+    event: React.FormEvent<HTMLFormElement>
+  ) {
+    event.preventDefault();
+    getRandomFacts(searchedAnimalType);
   }
 
   useEffect(() => {
-    getRandomFact();
+    getDefaultRandomFacts();
   }, []);
 
   return (
     <>
-      <h1>animal</h1>
-      <h2>Exemplos de fatos</h2>
+      <h1>Animal</h1>
+      <h2>Example dog facts</h2>
       <ul>
-        {dafaultFacts.map((fact) => (
-          <li>{fact}</li>
-        ))}
+        {defaultFacts.map((fact) => {
+          return (
+            <li key={fact._id}>
+              <p>{fact.text}</p>
+              <strong>{fact.updatedAt}</strong>
+            </li>
+          );
+        })}
+      </ul>
+
+      <h2>Search for new facts!</h2>
+
+      <form onSubmit={handleSubmitAnimalFactSearch}>
+        <label htmlFor="animal-type">Animal Type</label>
+        <input
+          type="text"
+          id="animal-type"
+          value={searchedAnimalType}
+          onChange={(event) => setSearchedAnimalType(event.target.value)}
+        />
+
+        <button>Search for facts</button>
+      </form>
+
+      {animalType && <h2>{animalType} facts</h2>}
+      <ul>
+        {facts.length > 0
+          ? facts.map((fact) => {
+              return (
+                <li key={fact._id}>
+                  <p>{fact.text}</p>
+                </li>
+              );
+            })
+          : animalType && (
+              <p>
+                No facts found for <strong>{animalType}</strong>
+              </p>
+            )}
       </ul>
     </>
   );
