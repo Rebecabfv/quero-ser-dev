6011979ce62a027edef81a72be8e1eefa1a89ada
Rebeca Baptista <re.bfviana@gmail.com>
2022-04-28 18:02:00 -0300

    add files thread

---
+++ java/thread/src/com/letscode/PrintService.java
@@ -0,0 +1,13 @@
+package com.letscode;
+
+public class PrintService {
+
+    private static final int NUM_TASKS = 10;
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= NUM_TASKS; i++) {
+            var task = new PrintTask(i);
+            task.print();
+        }
+    }
+}
---
+++ java/thread/src/com/letscode/PrintTask.java
@@ -0,0 +1,16 @@
+package com.letscode;
+
+public class PrintTask  {
+    private final String taskName;
+
+    // constructor
+    public PrintTask(final int taskId) {
+        this.taskName = "task_"+ taskId;
+    }
+
+    // print task name
+    public void print() {
+        System.out.printf("Task %s printed%n", taskName);
+    }
+
+}
