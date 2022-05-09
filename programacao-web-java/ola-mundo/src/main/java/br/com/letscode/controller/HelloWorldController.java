85ce8af17cbd2255ed8bd08ef4f465d4a6e94681
Rebeca Baptista <re.bfviana@gmail.com>
2022-05-09 16:09:02 -0300

    create methods post, put and delete

--- programacao-web-java/ola-mundo/src/main/java/br/com/letscode/controller/HelloWorldController.java
+++ programacao-web-java/ola-mundo/src/main/java/br/com/letscode/controller/HelloWorldController.java
@@ -1,12 +1,14 @@
 package br.com.letscode.controller;
 
 import br.com.letscode.controller.dto.HelloWorldDTO;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Log4j2
 public class HelloWorldController {
 
     @GetMapping(path = "/hello-world-antigo", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -19,7 +21,6 @@ public class HelloWorldController {
         HelloWorldDTO helloWorldDTO = new HelloWorldDTO();
         helloWorldDTO.setMensagem("Ola mundo");
         helloWorldDTO.setUsername("rebeca");
-
         return helloWorldDTO;
     }
 
@@ -28,7 +29,36 @@ public class HelloWorldController {
         var helloWorld = new HelloWorldDTO();
         helloWorld.setMensagem("Bem-vindo " + nome);
         helloWorld.setUsername(nome);
-
         return helloWorld;
+    }
+
+    @GetMapping(path = "/hello-world-response/{nome}")
+    public ResponseEntity<HelloWorldDTO> helloWorldFinal (@PathVariable(name = "nome") String nome){
+        var helloWorld = new HelloWorldDTO();
+        helloWorld.setUsername(nome);
+        helloWorld.setMensagem("Bem-vindo " + nome);
+        log.info("GET: "+helloWorld);
+        return new ResponseEntity(helloWorld, HttpStatus.OK);
+    }
+
+    //POST - envia com @RequestBody - return void e retorna um status de created
+    @PostMapping(path = "/hello-world-response")
+    public ResponseEntity<Void> postSimulation (@RequestBody HelloWorldDTO helloWorldDto){
+        log.info("POST: "+helloWorldDto);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    //PUT
+    @PutMapping(path = "/hello-world-response")
+    public ResponseEntity<Void> putSimulation(@RequestBody HelloWorldDTO helloWorldDto){
+        log.info("PUT: "+helloWorldDto);
+        return new ResponseEntity(HttpStatus.ACCEPTED);
+    }
+
+    //DELETE
+    @DeleteMapping(path = "/hello-world-response/{nome}")
+    public ResponseEntity<Void> deleteSimulation(@PathVariable(name = "nome") String nome){
+        log.info("APAGANDO: "+ nome);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
